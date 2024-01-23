34b02165da582286a090b83cda12ec770aa0b96a
Loc Le Phat <lpl212757@gmail.com>
2024-01-24 00:29:57 +0700

    MOD - modify highlight of current config

--- lua/base46/integrations/blankline.lua
+++ lua/base46/integrations/blankline.lua
@@ -3,6 +3,6 @@ local colors = require("base46").get_theme_tb "base_30"
 return {
   IndentBlanklineChar = { fg = colors.line },
   IndentBlanklineSpaceChar = { fg = colors.line },
-  IndentBlanklineContextChar = { fg = colors.grey },
+  IndentBlanklineContextChar = { fg = colors.baby_pink },
   IndentBlanklineContextStart = { bg = colors.one_bg2 },
 }
